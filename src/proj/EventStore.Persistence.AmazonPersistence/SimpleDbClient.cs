4e18350393fbe8c1aeb22170ad186e6ccde2d62f
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-03-24 06:47:15 -0600

    Starting to work with SimpleDB.

--- src/proj/EventStore.Persistence.AmazonPersistence/SimpleDbClient.cs
+++ src/proj/EventStore.Persistence.AmazonPersistence/SimpleDbClient.cs
@@ -7,7 +7,8 @@ namespace EventStore.Persistence.AmazonPersistence
 
 	public class SimpleDbClient
 	{
-		private const string ItemName = "{0}/{1}";
+		private const string PrimaryKey = "{0}/{1}";
+		private const string PayloadIdentifier = "{0}@{1}";
 		private const string RevisionQuery = "select * from {0} where StreamId = {1} and MinRevision >= {2} and MaxRevision <= {3} order by MinRevision";
 		private readonly AmazonSimpleDB client;
 
@@ -37,12 +38,54 @@ namespace EventStore.Persistence.AmazonPersistence
 				.WithExists(false);
 
 			var request = new PutAttributesRequest()
-				.WithDomainName("Commits") // TODO: sharding?
-				.WithItemName(ItemName.FormatWith(attempt.StreamId, attempt.CommitSequence))
+				.WithDomainName("Commits") // TODO: sharding
+				.WithItemName(PrimaryKey.FormatWith(attempt.StreamId, attempt.CommitSequence))
 				.WithExpected(optimisticConcurrency)
-				.WithAttribute(null);
+				.WithAttribute(GetAttributes(attempt, identifier, version));
 
+			// TODO: try/catch
+			this.client.PutAttributes(request);
 			return true;
+		}
+		private static ReplaceableAttribute[] GetAttributes(Commit attempt, string identifier, string version)
+		{
+			return new[]
+			{
+				new ReplaceableAttribute()
+					.WithName("MinStreamRevision")
+					.WithValue((attempt.StreamRevision - attempt.Events.Count).ToString())
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("MaxStreamRevision")
+					.WithValue(attempt.StreamRevision.ToString())
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("CommitId")
+					.WithValue(attempt.CommitId.ToString())
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("CommitSequence")
+					.WithValue(attempt.CommitSequence.ToString())
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("CommitStamp")
+					.WithValue(attempt.CommitStamp.ToString())
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("Dispatched")
+					.WithValue("false")
+					.WithReplace(false),
+
+				new ReplaceableAttribute()
+					.WithName("Payload")
+					.WithValue(PayloadIdentifier.FormatWith(identifier, version))
+					.WithReplace(false),
+			};
 		}
 
 		public virtual IEnumerable<Commit> Select(string domain, string streamId, int minRevision, int maxRevision)
